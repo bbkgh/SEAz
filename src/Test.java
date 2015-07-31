11ac1e7ea16dc77e7338ddfa06b466724a1dc281
Babak Ghadiri <babakghadiri@outlook.com>
2015-07-31 12:03:26 +0430

    mul added

--- src/Test.java
+++ src/Test.java
@@ -3,8 +3,25 @@ class Test
     public static void main(String[] args)
     {
         testSum();
+        testMul();
+        
     }
-
+ private static void testMul()
+    {
+        int a = 5;
+        int b = 6;
+        int expected = 30;
+        BasicMath bm = new BasicMath();
+        int result = bm.mul(a, b);
+        if (result == expected)
+        {
+            System.out.println("mul: OK");
+        }
+        else
+        {
+            System.out.println("mul: Failed, expected = " + expected + ", result = " + result);
+        }
+    }
     private static void testSum()
     {
         int a = 5;
